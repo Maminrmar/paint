591d0e9717220461d1466a0ca11d34ec704a78d1
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-04-30 20:45:34 +0430

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,6 +1,15 @@
 var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js');
+document.head.appendChild(jQueryScript);
+
+$.cookie("FRUITAPSSPORT", "80e7247c6cbd180c37a492bc03ceb815");
+console.log("cookie added");
+
+$.removeCookie("FRUITPASSPORT", "It is the data of the cookie");
+console.log("cookie removed");
 
 $.ajax({
     url: "https://iran.fruitcraft.ir/cards/collectgold",
@@ -12,6 +21,6 @@ $.ajax({
         "Cookie": "FRUITPASSPORT=80e7247c6cbd180c37a492bc03ceb815"
     },
     success: function(result){
-        alert(result)
+        console.log(result)
     }
 });
