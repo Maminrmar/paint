1d59c51d7b9529ad2f35261b4c1850c890ef4c9e
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-04-30 21:11:07 +0430

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,10 +1,6 @@
 var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
-if(typeof(FRUITPASSPORT)==undefined || typeof(TIMER)==undefined){
-    alert("Error!");
-    window.location.reload()
-}
 document.cookie = "FRUITPASSPORT="+FRUITPASSPORT+"; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/";
 function send(){
 $.ajax({
