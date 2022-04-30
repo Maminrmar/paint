2449ebc697d0bea9bf73285b81ff792d14bd41b5
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-04-30 21:00:37 +0430

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,8 +1,11 @@
 var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
-
-document.cookie = "FRUITPASSPORT=80e7247c6cbd180c37a492bc03ceb815; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/";
+if(typeof(FRUITPASSPORT)==undefined || typeof(TIMER)==undefined){
+    alert("Error!");
+    window.location.reload()
+}
+document.cookie = "FRUITPASSPORT="+FRUITPASSPORT+"; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/";
 function send(){
 $.ajax({
     url: "/cards/collectgold",
@@ -15,4 +18,4 @@ $.ajax({
     }
 });
 }
-setInterval("send()",1000);
+setInterval("send()",TIMER);
