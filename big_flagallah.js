555e7da91bfc3e43383ea59740ea7d35fb404cb0
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-05-03 16:35:33 +0430

    Update big_flagallah.js

--- big_flagallah.js
+++ big_flagallah.js
@@ -1440,7 +1440,8 @@ window.getPixelColor = function getPixelColor(x, y) {
         y = sety;
         PEN = setc;
         pok.classList.add("enabled");
-        pok.click()
+        //pok.click()
+        put();
     };
     
     window.getBoardFromXY = function getBoardFromXY(x, y, width, heigh) {
@@ -1465,6 +1466,20 @@ window.getPixelColor = function getPixelColor(x, y) {
         });
         return diff;
     };
+    window.put = function put(){
+	if(CD>Date.now())return
+	canvselect.style.background='';palette.style.transform='translateY(100%)';colors.children[PEN].classList.remove('sel');pok.classList.remove('enabled')
+	set(Math.floor(x), Math.floor(y), PEN)
+	canvselect.children[0].style.display='block';canvselect.style.outline='';canvselect.style.boxShadow=''
+	audios.cooldownStart.run()
+	CD = Date.now() + (localStorage.vip ? (localStorage.vip[0] == '!' ? 0 : COOLDOWN/2) : COOLDOWN)
+	let a = new DataView(new Uint8Array(6).buffer)
+	a.setUint8(0, 4)
+	a.setUint32(1, Math.floor(x) + Math.floor(y) * WIDTH)
+	a.setUint8(5, PEN)
+	PEN=-1
+	ws.send(a)
+    }
     window.doSingle = function doSingle() {
         const src = getBoardFromXY(
             destPosition.x,
@@ -1535,6 +1550,6 @@ window.getPixelColor = function getPixelColor(x, y) {
             document.body.removeChild(dialog);
         }, 8000);
     };
-    setInterval(() => !onCooldown && doSingle(), 50000);
+    setInterval(() => !onCooldown && doSingle(), 500);
     doSingle();
     
