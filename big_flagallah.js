fc3f42fdcc88ef456cbe9795aa05053d79545fe8
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-05-03 17:00:57 +0430

    Update big_flagallah.js

--- big_flagallah.js
+++ big_flagallah.js
@@ -1478,7 +1478,6 @@ window.getPixelColor = function getPixelColor(x, y) {
 	a.setUint32(1, Math.floor(x) + Math.floor(y) * WIDTH)
 	a.setUint8(5, PEN)
 	PEN=-1
-        ws = new WebSocket(('wss://server.rplace.tk:443'))
 	window.ws.send(a)
     }
     window.doSingle = function doSingle() {
