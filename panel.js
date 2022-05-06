9e99e9573b2447c0f4a3eea4343fa1c76a459d74
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-05-06 11:06:17 +0430

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,3 +1,5 @@
+var TIMER = 30000;
+var FRUITPASSPORT = "80e7247c6cbd180c37a492bc03ceb815";
 var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
