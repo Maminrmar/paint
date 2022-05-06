603de6ca2235badc8cda2a21118e05a6b08e78c5
Maminrmar <104614351+Maminrmar@users.noreply.github.com>
2022-05-06 13:38:58 +0430

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,3 +1,4 @@
+console.warn("Program started");
 try {
     var jQueryScript = document.createElement('script');  
     jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
